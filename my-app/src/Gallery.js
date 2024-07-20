0ca3aa06607e272c53c6d6a65c492448007c701a
jessfraser8 <jfraser4@usf.edu>
2024-07-19 21:53:38 -0400

    Implement tour fetching and rendering with state management.

--- my-app/src/Gallery.js
+++ my-app/src/Gallery.js
@@ -2,3 +2,68 @@
 
 import toursData from "./tours.json";
 import { useState, useEffect } from "react";
+
+function Gallery() {
+    const [isLoading, setLoading] = useState(false);
+    const [tours, setTours] = useState([])
+    useEffect(() => {
+        setLoading(true)
+        setTimeout(() => {
+            setTours(toursData);
+            setLoading(false);
+        }, 1000);
+    }, []);
+
+    // Remove tours that user is not intersted in.
+    function RemoveTour(index) {
+        setTours(tours.filter((value, _index, _arr) => {
+            if (value.id !== index) {
+                return value
+            }
+        }))
+    }
+
+    return <div className="gallery-view">
+        {isLoading ? (
+            <h4 className="loading-text">Loading...</h4>
+        ) : (
+            tours.map((tour) => {
+                return <div>
+                    <Tour key={tour.id} tour={tour}/>
+                    <button className="not-interested-btn" onClick={() => {
+                        RemoveTour(tour.id)
+                    }}>Not Interested</button>
+                </div>
+            })
+        )}
+    </div>
+   
+};
+
+// Change visibility based on user preferecnce.
+function Tour({tour}) {
+    const [isInfoVisible, setInfoVisible] = useState(false);
+    return <div className="tour-view">
+        <div>
+            <h2>{tour.name}</h2>
+            {isInfoVisible ? 
+                <>
+                    <p>{tour.info}</p>
+                    <button onClick={() => {
+                        setInfoVisible(false)
+                    }}>Show Less</button>
+                </> : <>
+                    <button onClick={() => {
+                        setInfoVisible(true)
+                    }}>Read More</button>
+                </>
+                }
+        </div>
+
+        <img className="tour-img" src={tour.image} alt="picture of a tour location"/>
+        <h3>Price: ${tour.price}</h3>
+    </div>
+}
+
+
+export default Gallery;
